8c4bb4643a8d42dbf5e47fb8078e7fb5cb3832c7
Alexey Zarazhevsky <alexey.zarazhevsky@magenta-technology.com>
2018-02-13 13:00:20 +0400

    first quest

---
+++ home/src/main/java/com/cool/boy/Variables.java
@@ -0,0 +1,18 @@
+package com.cool.boy;
+
+/*
+Объявляем переменные
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+
+Выведи на экран в формате:
+Имя: {name}, возраст: {age}, город: {city}
+
+*/
+
+public class Variables {
+
+    public static void main(String... args){
+        //your code here
+    }
+}
